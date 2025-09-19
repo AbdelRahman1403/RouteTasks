095a1250556fc535f02bd72f9f2c3d26628693a4
AbdelRahman_Yassin <abdelrahman.ahmed.yassin03@gmail.com>
2025-09-19 03:38:04 +0300

    Csharp advanced 4

---
+++ CSharpAdvanced/Session4/ConsoleApp1/Book.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Book
+    {
+        public string? ISBN { get; set; }
+        public string? Title { get; set; }
+        public string[]? Author { get; set; }
+        public DateTime YearPublished { get; set; }
+        public decimal Price { get; set; }
+        public Book(string? iSBN, string? title, string[]? author, DateTime yearPublished, decimal price)
+        {
+            ISBN = iSBN;
+            Title = title;
+            Author = author;
+            YearPublished = yearPublished;
+            Price = price;
+        }
+
+        public override string ToString()
+        {
+            return $"ID : {ISBN} , Title : {Title}\n" +
+                   $"The Authors : {string.Join(", ", Author ?? Array.Empty<string>())}\n" +
+                   $"The YearPublished : {YearPublished.Month}/{YearPublished.Year}"+
+                   $"The price is : {Price:C}";
+
+        }
+    }
+
+    internal class BookFunctions 
+    {
+        public static string GetTitle(Book k)
+        {
+            return k.Title ?? string.Empty;
+        }
+        public static string GetAuthors (Book k)
+        {
+            return string.Join(", " , k.Author ?? Array.Empty<string>());
+        }
+
+        public static decimal GetPrice (Book k)
+        {
+            return k.Price;
+        }
+    }
+    internal class LibraryEngine
+    {
+//        public delegate TResult BookDelegate<out TResult>(Book b);
+        //public static void ProcessBooks<T>(List<Book> BList , BookDelegate<T> bookDelegate)
+        //{
+        //    foreach (var book in BList)
+        //    {
+        //        var result = bookDelegate(book);
+        //        Console.WriteLine(result);
+        //    }
+        //}
+        public static void ProcessBooks<T>(List<Book> BList, Func<Book , T> bookDelegate)
+        {
+            foreach (var book in BList)
+            {
+                var result = bookDelegate(book);
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
---
+++ CSharpAdvanced/Session4/ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace ConsoleApp1
+{
+    
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Question 1
+
+            //Book b1 = new Book("978-0-13-110362-7", "The C Programming Language", new string[] { "Brian W. Kernighan", "Dennis M. Ritchie" }, new DateTime(1988, 3, 22), 65.99m);
+
+            //Console.WriteLine(b1);
+            //Console.WriteLine(BookFunctions.GetPrice(b1));
+            //Console.WriteLine(BookFunctions.GetTitle(b1));
+            //Console.WriteLine(BookFunctions.GetAuthors(b1));
+            #endregion
+
+            Book b1 = new Book("978-0-13-110362-7", "The C Programming Language", new string[] { "Brian W. Kernighan", "Dennis M. Ritchie" }, new DateTime(1988, 3, 22), 65.99m);
+            Book b2 = new Book("978-0-201-83595-3", "Introduction to Algorithms", new string[] { "Thomas H. Cormen", "Charles E. Leiserson", "Ronald L. Rivest", "Clifford Stein" }, new DateTime(2009, 7, 31), 89.99m);
+            Book b3 = new Book("978-0-596-52068-7", "Learning Python", new string[] { "Mark Lutz" }, new DateTime(2013, 6, 12), 54.99m);
+            List<Book> Books = new List<Book> { b1, b2, b3 };
+            //Build in delegate
+            LibraryEngine.ProcessBooks(Books , BookFunctions.GetPrice);
+            //Using Anonymous Method
+            LibraryEngine.ProcessBooks(Books, delegate (Book b) { return b.ISBN; });
+
+            //Using Lambda Expression
+            LibraryEngine.ProcessBooks(Books,(Book b) => b.YearPublished.Year);
+
+        }
+    }
+}
